0c0c4b9b2cd0a8ef7e030ae88b84062c41afb7cf
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-25 23:22:42 +0000

    solution

--- main.py
+++ main.py
@@ -5,3 +5,22 @@
 #If n is even and in the inclusive range of 6 to 20, print Weird
 #If n is even and greater than 20, print Not Weird
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if (n%2!=0):
+        print("Weird")
+    else:
+        if (n in range(2,6) or n>20):
+            print("Not Weird")
+        if (n in range(6,21)):
+            print("Weird")
